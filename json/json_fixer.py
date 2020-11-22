6f26128a0bf03938ad5a4ba4e25f9444e5836298
Dmitrii <yndx-dvornikoff@ya.ru>
2020-11-22 18:52:21 +0300

    JSON немного переформатирован: объекты типа "монстр" упакованы в список.

---
+++ json/json_fixer.py
@@ -0,0 +1,17 @@
+def main():
+    old_file = open('monsters_old.json', 'rt', encoding='utf8')
+    new_file = open('monsters.json', 'wt', encoding='utf8')
+
+    for line in old_file:
+        if line.__contains__('monster') & line.__contains__(':'):
+            new_line = '    {\n'
+        else:
+            new_line = line
+        new_file.write(new_line)
+
+    old_file.close()
+    new_file.close()
+
+
+if __name__ == '__main__':
+    main()
