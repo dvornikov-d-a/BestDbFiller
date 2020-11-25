88dce189bf0955da6d6e9c777232c09b072444c2
Dmitrii <yndx-dvornikoff@ya.ru>
2020-11-25 03:49:39 +0300

    Добавлен класс "монстр" (для JSON)

--- json_work/json_fixer.py
+++ json_work/json_fixer.py
@@ -3,8 +3,10 @@ new_file = open('monsters.json', 'wt', encoding='utf8')
 for line in old_file:
     # if line.__contains__('monster') & line.__contains__(':'):
     #     new_line = '    {\n'
-    if line.__contains__('ability'):
-        new_line = line.replace('ability', 'agility')
+    # if line.__contains__('ability'):
+    #     new_line = line.replace('ability', 'agility')
+    if line.__contains__('feeling'):
+        new_line = line.replace('feeling', 'feelings')
     else:
         new_line = line
     new_file.write(new_line)
---
+++ json_work/monster.py
@@ -0,0 +1,20 @@
+class Monster(object):
+    def __init__(self, name, armor_class, hits, speed, strength, agility, physique, intellect, wisdom,
+                 charisma, skills, feelings, danger, abilities, is_player, actions, description):
+        self.name = name
+        self.armor_class = armor_class
+        self.hits = hits
+        self.speed = speed
+        self.strength = strength
+        self.agility = agility
+        self.physique = physique
+        self.intellect = intellect
+        self.wisdom = wisdom
+        self.charisma = charisma
+        self.skills = skills
+        self.feelings = feelings
+        self.danger = danger
+        self.abilities = abilities
+        self.is_player = is_player
+        self.actions = actions
+        self.description = description
