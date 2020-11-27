f4397c607602a12009c8271460352608f2cca80d
Dmitrii <yndx-dvornikoff@ya.ru>
2020-11-28 00:37:42 +0300

    INSERT'ы 2.0
    Добавлены функции вставки данных о сущностях, скоростях

--- db/db_worker.py
+++ db/db_worker.py
@@ -20,6 +20,8 @@ class DbWorker(object):
             active_actions_ids = self._insert_active_actions(db_data.active_actions, connection)
             armors_ids = self._insert_armors(db_data.armors, connection)
             skills_ids = self._insert_skills(db_data.skills, connection)
+            feelings_ids = self._insert_feelings(db_data.feelings, connection)
+            entities_ids = self._insert_entities(db_data.entities, stats_ids, armors_ids, connection)
 
     def _insert_stats(self, stats, connection):
         stats_ids = []
@@ -61,8 +63,8 @@ class DbWorker(object):
             monster_abilities_ids = []
 
             for ability in monster_abilities:
-                name = self._null_or_value(ability.name)
-                info = self._null_or_value(ability.info)
+                name = self._null_or_str(ability.name)
+                info = self._null_or_str(ability.info)
                 insert_query = f'INSERT INTO {abilities_table_name} ' \
                                f'({abilities_name_col_name}, {abilities_info_col_name}) ' \
                                f'VALUES ' \
@@ -80,8 +82,8 @@ class DbWorker(object):
         for monster_languages in languages:
             monster_languages_ids = []
             for language in monster_languages:
-                name = self._null_or_value(languages.name)
-                info = self._null_or_value(languages.info)
+                name = self._null_or_str(languages.name)
+                info = self._null_or_str(languages.info)
 
                 insert_query = f'INSERT INTO {languages_table_name} ' \
                                f'({languages_name_col_name}, {languages_info_col_name}) ' \
@@ -98,8 +100,8 @@ class DbWorker(object):
         for monster_active_actions in active_actions:
             monster_active_actions_ids = []
             for active_action in monster_active_actions:
-                name = self._null_or_value(active_action.name)
-                desc = self._null_or_value(active_action.desc)
+                name = self._null_or_str(active_action.name)
+                desc = self._null_or_str(active_action.desc)
                 insert_query = f'INSERT INTO {active_actions_table_name} ' \
                                f'({active_actions_name_col_name}, {active_actions_desc_col_name}) ' \
                                f'VALUES ' \
@@ -114,8 +116,8 @@ class DbWorker(object):
         armors_ids = []
         for armor in armors:
             class_ = self._parse_int_from_str(armor.class_)
-            type_ = self._null_or_value(armor.type)
-            extra = self._null_or_value(armor.extra)
+            type_ = self._null_or_str(armor.type)
+            extra = self._null_or_str(armor.extra)
             insert_query = f'INSERT INTO {armors_table_name} ' \
                            f'({armors_class_col_name}, {armors_type_col_name}, {armors_extra_col_name}) ' \
                            f'VALUES ' \
@@ -130,7 +132,7 @@ class DbWorker(object):
         for monster_skills in skills:
             monster_skills_ids = []
             for skill in monster_skills:
-                name = self._null_or_value(skill.name)
+                name = self._null_or_str(skill.name)
                 buff = self._parse_int_from_str(skill.buff)
                 insert_query = f'INSERT INTO {skills_table_name} ' \
                                f'({skills_name_col_name}, {skills_buff_col_name}) ' \
@@ -147,8 +149,8 @@ class DbWorker(object):
         for monster_feelings in feelings:
             monster_feelings_ids = []
             for feeling in monster_feelings:
-                name = self._null_or_value(feeling.name)
-                buff = self._null_or_value(feeling.buff)
+                name = self._null_or_str(feeling.name)
+                buff = self._null_or_str(feeling.buff)
                 if feeling.radius != 'NULL':
                     radius = self._parse_int_from_str(feeling.radius)
                 else:
@@ -163,16 +165,60 @@ class DbWorker(object):
             feelings_ids.append(monster_feelings_ids)
         return feelings_ids
 
+    def _insert_entities(self, entities, stats_ids, armors_ids, connection):
+        entities_ids = []
+        for index, entity in zip(range(len(entities)), entities):
+            name = self._null_or_str(entity.name)
+            hp = self._null_or_int(entity.hp)
+            hits = self._null_or_str(entity.hits)
+            danger = self._null_or_str(entity.danger)
+            desc = self._null_or_str(entity.desc)
+            exp = self._null_or_int(entity.exp)
+            stats = stats_ids[index]
+            armor = armors_ids[index]
+            insert_query = f'INSERT INTO {entities_table_name} ' \
+                           f'({entities_name_col_name}, {entities_hp_col_name}, {entities_hits_col_name},' \
+                           f' {entities_danger_col_name}, {entities_desc_col_name}, {entities_exp_col_name},' \
+                           f' {entities_stats_col_name}, {entities_armor_col_name}) ' \
+                           f'VALUES ' \
+                           f'({name}, {hp}, {hits}, {danger}, {desc}, {exp}, {stats}, {armor}) ' \
+                           f'RETURNING id;'
+            entity_id = self._insert_returning_id(insert_query, connection)
+            entities_ids.append(entity_id)
+        return entities_ids
+
+    def __insert_speeds(self, speeds, entities_ids, connection):
+        speeds_ids = []
+        for entity, monster_speeds in zip(entities_ids, speeds):
+            monster_speeds_ids = []
+            for speed in monster_speeds:
+                type_ = self._null_or_str(speed.type)
+                value = self._null_or_int(speed.value)
+                insert_query = f'INSERT INTO {speeds_table_name} ' \
+                               f'({speeds_entity_col_name}, {speeds_type_col_name}, {speeds_value_col_name}) ' \
+                               f'VALUES ' \
+                               f'({entity}, {type_}, {value}) ' \
+                               f'RETURNING id;'
+                speed_id = self._insert_returning_id(insert_query, connection)
+                monster_speeds_ids.append(speed_id)
+            speeds_ids.append(monster_speeds_ids)
+        return speeds_ids
+
     @staticmethod
     def _insert_returning_id(insert_query, connection, id_col_name='id'):
         return connection.execute(insert_query).first()[id_col_name]
+
+    def _null_or_int(self, string):
+        if string == 'NULL':
+            return string
+        return self._parse_int_from_str(string)
 
     @staticmethod
     def _parse_int_from_str(string):
         return int(''.join([symbol for symbol in string if symbol.isdigit()]))
 
     @staticmethod
-    def _null_or_value(string):
+    def _null_or_str(string):
         if string == 'NULL':
             return string
         return f"'{string}'"
